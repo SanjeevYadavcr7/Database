bad68fc7734ee1bc063dfd23d074c1b2a54cb939
Sanjeev Yadav <sanjeevyadavcr7@gmail.com>
2025-09-29 20:06:46 +0530

    doc(orm): prisma

---
+++ Prisma/dist/create-user.js
@@ -0,0 +1,26 @@
+import { PrismaClient } from '../generated/prisma/index.js';
+import { withAccelerate } from '@prisma/extension-accelerate';
+const prisma = new PrismaClient().$extends(withAccelerate());
+async function main() {
+    const users = await prisma.user.findMany({});
+    console.log(users);
+    const user = await prisma.user.findUnique({
+        where: {
+            id: 1
+        },
+        include: {
+            posts: true
+        }
+    });
+    console.log(user);
+}
+main()
+    .then(async () => {
+    await prisma.$disconnect();
+})
+    .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    // process.exit(1)
+});
+//# sourceMappingURL=create-user.js.map
